ca7d615d22f0633aa17e088c643d6b0d327f027e
Mayur-Bk <mayurbelgaumkar03@gmail.com>
2024-07-15 18:58:05 +0530

    Hyperparameter tuning

--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,7 @@ import pandas as pd
 import dill
 #import pickle
 from sklearn.metrics import r2_score
-#from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -22,21 +22,21 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train,X_test,y_test,models,params):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
-#            para=param[list(models.keys())[i]]
+            para=params[list(models.keys())[i]]
 
-#            gs = GridSearchCV(model,para,cv=3)
-#            gs.fit(X_train,y_train)
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
 
-#            model.set_params(**gs.best_params_)
-#            model.fit(X_train,y_train)
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
 
-            model.fit(X_train, y_train)  # Train model
+#            model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
 
